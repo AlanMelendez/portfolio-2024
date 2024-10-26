471e8607cd3c0211d3a01f03673f337c6e16564f
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-10-25 23:58:45 -0600

    Preview main screen success

---
+++ src/app/components/home/components/about-me/about-me.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `
+    <!-- <div class="portfolio-container">
+      <img src="/img/me-one.png" alt="Tu imagen" class="profile-image" />
+      <h1 class="portfolio-title">Ing. Alan Cuevas Melendez</h1>
+      <p class="portfolio-description">
+      Hola, soy Alan. 👋 Es de un placer tenerte por aqui, cuento con más de tres años de experiencia en el desarrollo de soluciones tecnológicas de alto impacto. Actualmente trabajo como desarrollador, especializándome en la creación de aplicaciones frontend, backend y móviles híbridas. Mi enfoque se centra en diseñar e implementar sistemas que cumplan con los más altos estándares de rendimiento y escalabilidad, aplicando siempre las mejores prácticas para optimizar la experiencia del usuario y garantizar la eficiencia en cada proyecto.
+
+</div> -->
+
+<div class="portfolio-container">
+  <!-- Contenedor Izquierdo -->
+    <div class="left-container">
+      <div class="container-img">
+        <img src="/img/me-one.png" alt="Tu imagen" class="profile-image" />
+      </div>
+      <div class="container-name">
+        <h1 class="portfolio-title">Ing. Alan Cuevas Melendez</h1>
+      </div>
+      <div class="social-links">
+        <a href="https://github.com/yourusername" target="_blank" style="background-color: white;">
+          <img src="/icons/github.png" alt="GitHub" class="img-social" />
+        </a>
+        <a href="https://instagram.com/yourusername" target="_blank">
+          <img src="/icons/gmail.png" alt="Gmail" class="img-social"  />
+        </a>
+        <a href="https://linkedin.com/in/yourusername" target="_blank">
+          <img src="/icons/linkedin.png" alt="LinkedIn" class="img-social"  />
+        </a>
+      </div>
+
+
+  </div>
+
+  <!-- Contenedor Derecho -->
+  <div class="right-container">
+    <p class="portfolio-description">
+    Hola, soy Alan. Soy Ingeniero en Informática con más de tres años de experiencia como Desarrollador Full Stack, especializado en aplicaciones web y móviles híbridas. Me enfoco en diseñar soluciones de alto rendimiento aplicando buenas prácticas, como Clean Code y arquitecturas escalables, para proyectos que integran tecnologías como Angular, Ionic, Laravel y .NET.
+    </p>
+    <button class="portfolio-button">Ir a Proyectos</button>
+
+  </div>
+</div>
+
+
+
+  `,
+  styleUrl: './about-me.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutMeComponent { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionPanel } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule, MatNavList, MatListItem } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AboutMeComponent } from "./components/about-me/about-me.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
-  templateUrl: './home.component.html',
+  imports: [MatToolbarModule, MatIconModule, MatFormFieldModule, MatSidenavModule,
+    MatListModule, MatButtonModule, MatNavList, MatListItem, MatExpansionPanel, FooterComponent, AboutMeComponent],  templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  titleIcon:string=`<AlanMelendez/>`;
 
+  constructor() { }
+
+  fields = [
+    { icon: 'home', textMX:'Acerca de mi' , textUS: 'Home', path: '/home' },
+    { icon: 'info',textMX:'Experiencia' , textUS: 'About me', path: '/about' },
+    { icon: 'list',textMX:'Proyectos' , textUS: 'Projects', path: '/projects' },
+    { icon: 'add', textMX:'Cursos' , textUS: 'Course', path: '/crourse' },
+  ];
 }
