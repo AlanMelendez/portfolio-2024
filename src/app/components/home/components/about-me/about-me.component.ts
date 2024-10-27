828baa58d4b9c37e5ad8eaff69b9922e3248414a
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-10-26 22:14:47 -0600

    Se ajustaron los iconos junto ala foto de perfil

--- src/app/components/home/components/about-me/about-me.component.ts
+++ src/app/components/home/components/about-me/about-me.component.ts
@@ -18,37 +18,35 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 
 <div class="portfolio-container">
   <!-- Contenedor Izquierdo -->
-    <div class="left-container">
-      <div class="container-img">
-        <img src="/img/me-one.png" alt="Tu imagen" class="profile-image" />
-      </div>
-      <div class="container-name">
-        <h1 class="portfolio-title">Ing. Alan Cuevas Melendez</h1>
-      </div>
-      <div class="social-links">
-        <a href="https://github.com/AlanMelendez" target="_blank" style="background-color: white;">
+  <div class="left-container">
+    <div class="container-img">
+      <img src="/img/me-one.png" alt="Tu imagen" class="profile-image" />
+      <div class="social-icons">
+        <a href="https://github.com/AlanMelendez" target="_blank">
           <img src="/icons/github.png" alt="GitHub" class="img-social" />
         </a>
         <a href="mailto:alanmelendezalaan@outlook.com" target="_blank">
-          <img src="/icons/gmail.png" alt="Gmail" class="img-social"  />
+          <img src="/icons/gmail.png" alt="Gmail" class="img-social" />
         </a>
         <a href="https://linkedin.com/in/alan-melendez-bb3537218" target="_blank">
-          <img src="/icons/linkedin.png" alt="LinkedIn" class="img-social"  />
+          <img src="/icons/linkedin.png" alt="LinkedIn" class="img-social" />
         </a>
       </div>
-
-
+    </div>
+    <div class="container-name">
+      <h1 class="portfolio-title">Ing. Alan Cuevas Melendez</h1>
+    </div>
   </div>
 
   <!-- Contenedor Derecho -->
   <div class="right-container">
     <p class="portfolio-description">
-    Hola, soy Alan. Soy Ingeniero en Informática con más de tres años de experiencia como Desarrollador Full Stack, especializado en aplicaciones web y móviles híbridas. Me enfoco en diseñar soluciones de alto rendimiento aplicando buenas prácticas, como Clean Code y arquitecturas escalables, para proyectos que integran tecnologías como Angular, Ionic, Laravel y .NET.
+      Hola, soy Alan. Soy Ingeniero en Informática con más de tres años de experiencia como Desarrollador Full Stack, especializado en aplicaciones web y móviles híbridas. Me enfoco en diseñar soluciones de alto rendimiento aplicando buenas prácticas, como Clean Code y arquitecturas escalables, para proyectos que integran tecnologías como Angular, Ionic, Laravel y .NET.
     </p>
     <button class="portfolio-button">Ir a Proyectos</button>
-
   </div>
 </div>
+
 
 
 
