17f79d57cdc48a316925cb454f8fb107d8c97186
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-12-27 05:31:20 -0600

    Modify modals style to buttons

--- src/app/components/home/components/about-me/about-me.component.ts
+++ src/app/components/home/components/about-me/about-me.component.ts
@@ -71,7 +71,6 @@ import {
             <a
               href="https://alancuevasmelendez.com/cv/AlanCuevasMelendez.pdf"
               target="_blank"
-              download
               class="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 hover:scale-105 transition-transform duration-300"
             >
               Descargar CV
